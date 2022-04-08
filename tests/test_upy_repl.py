dae15c04512f6e25e93846e955798d56efa79d50
Jonathan Heathcote <mail@jhnet.co.uk>
2022-04-08 22:21:40 +0100

    Add low-level upy_repl module

---
+++ tests/test_upy_repl.py
@@ -0,0 +1,375 @@
+from typing import Optional
+
+import pytest
+
+import time
+
+from serial import Serial
+
+from unittest.mock import Mock
+
+from textwrap import dedent
+
+from contextlib import contextmanager
+
+from upyt.upy_repl import (
+    MicroPythonReplError,
+    expect,
+    expect_endswith,
+    interrupt_and_enter_repl,
+    raw_mode,
+    raw_paste_exec,
+    soft_reset_directly_into_repl,
+)
+
+
+class TestExpect:
+
+    def test_matches(self) -> None:
+        ser = Mock()
+        ser.read.return_value = b"yes"
+        
+        assert expect(ser, b"yes") == b"yes"
+
+    def test_no_match(self) -> None:
+        ser = Mock()
+        ser.read.return_value = b"no"
+        
+        with pytest.raises(MicroPythonReplError) as exc_info:
+            expect(ser, b"yes")
+        
+        assert exc_info.value.args[0] == b"no"
+
+
+class TestExpectEndswIth:
+
+    def test_matches(self) -> None:
+        ser = Mock()
+        ser.read_until.return_value = b"well...yes"
+        
+        assert expect_endswith(ser, b"yes") == b"well...yes"
+
+    def test_no_match(self) -> None:
+        ser = Mock()
+        ser.read_until.return_value = b"well...no"
+        
+        with pytest.raises(MicroPythonReplError) as exc_info:
+            expect_endswith(ser, b"yes")
+        
+        assert exc_info.value.args[0] == b"well...no"
+
+
+class TestInterruptAndEnterRepl:
+    
+    def test_no_contention_next_prompt_unread(self, ser: Serial) -> None:
+        interrupt_and_enter_repl(ser)
+        
+        # Confirm the REPL is working and that we've gone back to a prompt
+        ser.write(b"print('hello')\r")
+        assert ser.read_until(b"\r\nhello") == (
+            b"print('hello')\r\n"  # Echo
+            b"hello"  # Output
+        )
+        
+        # NB: newline and prompt (>>>) still left in buffer
+        assert interrupt_and_enter_repl(ser) == b"\r\n>>> "
+    
+    def test_no_contention_next_prompt_read(self, ser: Serial) -> None:
+        interrupt_and_enter_repl(ser)
+        
+        # Confirm the REPL is working and that we've gone back to a prompt
+        ser.write(b"print('hello')\r")
+        assert ser.read_until(b"hello\r\n>>> ") == (
+            b"print('hello')\r\n"  # Echo
+            b"hello\r\n"  # Output
+            b">>> "  # Next prompt
+        )
+        
+        # NB: nothing left in buffer
+        assert interrupt_and_enter_repl(ser) == b""
+    
+    def test_interrupt_process(self, ser: Serial) -> None:
+        interrupt_and_enter_repl(ser)
+        
+        before = time.time()
+        
+        ser.write(b"import time; time.sleep(3)\r")
+        
+        # XXX: Due to an unknown issue with the ACM driver/pyserial/something
+        # else(?), if we do not wait for the echo to come back, the attempt to
+        # write a ^C character in interrupt_and_enter_repl() will be blocked
+        # until the end of the sleep for reasons unknown.
+        expect(ser, b"import time; time.sleep(3)\r\n")
+        
+        # Ensure we interrupt the sleep
+        out = interrupt_and_enter_repl(ser)
+        print(out.decode("utf-8"))
+        after = time.time()
+        
+        # Make sure our ^C worked (and that we didn't end up blocked waiting
+        # for the printout
+        assert after - before < 0.5
+        
+        assert out == (
+            b"Traceback (most recent call last):\r\n"
+            b'  File "<stdin>", line 1, in <module>\r\n'
+            b"KeyboardInterrupt: "
+        )
+    
+    def test_interrupt_process_custom_message(self, ser: Serial) -> None:
+        interrupt_and_enter_repl(ser)
+        
+        before = time.time()
+        
+        # XXX: Due to an unknown issue with the ACM driver/pyserial/something
+        # else(?), if we do not wait for the echo to come back, the attempt to
+        # write a ^C character in interrupt_and_enter_repl() will be blocked
+        # until the end of the sleep for reasons unknown.
+        ser.write(b"import time\r")
+        expect(ser, b"import time\r\n")
+        ser.write(b"try: time.sleep(3)\r")
+        expect(ser, b">>> try: time.sleep(3)\r\n")
+        ser.write(b"except: print('stopped\\ndead')\r")
+        expect(ser, b"... except: print('stopped\\ndead')\r\n")
+        ser.write(b"\r")
+        expect(ser, b"... \r\n")
+        
+        # Ensure we interrupt the sleep
+        out = interrupt_and_enter_repl(ser)
+        after = time.time()
+        
+        # Make sure our ^C worked (and that we didn't end up blocked waiting
+        # for the printout
+        assert after - before < 0.5
+        
+        assert out == (
+            b"stopped\r\n"
+            b"dead"
+        )
+    
+    def test_double_interrupt_process(self, ser: Serial) -> None:
+        interrupt_and_enter_repl(ser)
+        
+        before = time.time()
+        
+        ser.write(b"import time\r")
+        expect(ser, b"import time\r\n>>> ")
+        ser.write(b"try: time.sleep(3)\r")
+        expect(ser, b"try: time.sleep(3)\r\n... ")
+        ser.write(b"finally: time.sleep(2)\r")
+        expect(ser, b"finally: time.sleep(2)\r\n... ")
+        ser.write(b"\r")
+        expect(ser, b"\r\n")
+        
+        # XXX: Due to an unknown issue with the ACM driver/pyserial/something
+        # else(?), if we do not wait for the echo to come back, the attempt to
+        # write a ^C character in interrupt_and_enter_repl() will be blocked
+        # until the end of the sleep for reasons unknown.
+        
+        # Ensure we interrupt the sleep
+        out = interrupt_and_enter_repl(ser)
+        after = time.time()
+        
+        # Make sure our ^C worked (and that we didn't end up blocked waiting
+        # for the printout
+        delta = after - before
+        assert delta < 0.5
+        
+        assert out == (
+            b"Traceback (most recent call last):\r\n"
+            b'  File "<stdin>", line 2, in <module>\r\n'
+            b"KeyboardInterrupt: "
+        )
+
+
+def test_raw_mode(ser: Serial) -> None:
+    with raw_mode(ser):
+        # Check we're actually in raw mode
+        ser.write(
+            b"print('hello')"
+            b"\x04"  # (Ctrl+D)
+        )
+        expect(ser, b"OKhello\r\n\x04\x04")
+    
+    # Check returned to non-raw mode
+    ser.write(b"print('hello')\r")
+    expect(ser, b"print('hello')\r\n")  # Echo
+    expect(ser, b"hello\r\n")
+    expect(ser, b">>> ")  # New prompt
+
+
+class TestRawPasteExec:
+
+    def test_basic_functionality_no_exception(self, ser: Serial) -> None:
+        with raw_mode(ser):
+            out, err = raw_paste_exec(
+                ser,
+                dedent(
+                    r"""
+                        print("hello\nworld!")
+                        print("goodbye!")
+                    """
+                ),
+            )
+            assert err == ""
+            assert out == (
+                "hello\r\n"
+                "world!\r\n"
+                "goodbye!\r\n"
+            )
+
+    def test_basic_functionality_with_exception(self, ser: Serial) -> None:
+        with raw_mode(ser):
+            out, err = raw_paste_exec(
+                ser,
+                dedent(
+                    r"""
+                        print("success")
+                        raise Exception("Failure!")
+                    """
+                ),
+            )
+            assert out == "success\r\n"
+            assert err == (
+                "Traceback (most recent call last):\r\n"
+                '  File "<stdin>", line 3, in <module>\r\n'
+                "Exception: Failure!\r\n"
+            )
+
+    def test_input_flow_control_required(self, ser: Serial) -> None:
+        with raw_mode(ser):
+            out, err = raw_paste_exec(
+                ser,
+                f"print({'+'.join('1' for _ in range(1000))})"
+            )
+            assert err == ""
+            assert out == "1000\r\n"
+
+    def test_input_flow_control_size_multiple_of_window(self, ser: Serial) -> None:
+        # XXX: Hard coded based on what the Pi Pico defaults to...
+        window_size = 128
+        
+        with raw_mode(ser):
+            out, err = raw_paste_exec(
+                ser,
+                f"print(123)".ljust(window_size * 8, "#")
+            )
+            assert err == ""
+            assert out == "123\r\n"
+
+    def test_disallow_ctrl_d_in_source(self, ser: Serial) -> None:
+        with raw_mode(ser):
+            with pytest.raises(ValueError):
+                raw_paste_exec(ser, "oh noes\x04")
+
+
+
+def quick_and_dirty_write_file(ser: Serial, file: str, value: Optional[str]) -> None:
+    interrupt_and_enter_repl(ser)
+    
+    with raw_mode(ser):
+        if value is not None:
+            assert raw_paste_exec(
+                ser,
+                dedent(
+                    f"""
+                        with open({file!r}, "w") as f:
+                            f.write({value!r})
+                    """
+                ).strip()
+            ) == ("", "")
+        else:
+            assert raw_paste_exec(
+                ser,
+                dedent(
+                    f"""
+                        import os
+                        try:
+                            os.remove({file!r})
+                        except OSError:
+                            pass
+                    """
+                ).strip()
+            ) == ("", "")
+
+
+def quick_and_dirty_read_file(ser: Serial, file: str) -> Optional[str]:
+    interrupt_and_enter_repl(ser)
+    
+    with raw_mode(ser):
+        out, err = raw_paste_exec(
+            ser,
+            dedent(
+                f"""
+                    try:
+                        print(repr(open({file!r}, "r").read()))
+                    except OSError:
+                        print("None")
+                """
+            ).strip()
+        )
+    
+    assert err == ""
+    return eval(out)
+
+
+@contextmanager
+def quick_and_dirty_override_file(ser: Serial, file: str, content: Optional[str]):
+    original = quick_and_dirty_read_file(ser, file)
+    quick_and_dirty_write_file(ser, file, content)
+    try:
+        yield
+    finally:
+        quick_and_dirty_write_file(ser, file, original)
+
+
+@pytest.mark.parametrize(
+    "original, replacement",
+    [
+        ("foo", "bar"),
+        ("foo", None),
+        (None, "foo"),
+        (None, None),
+    ],
+)
+def test_override_file(
+    ser: Serial,
+    original: Optional[str],
+    replacement: Optional[str],
+) -> None:
+    quick_and_dirty_write_file(ser, "file.txt", original)
+    
+    with quick_and_dirty_override_file(ser, "file.txt", replacement):
+        assert quick_and_dirty_read_file(ser, "file.txt") == replacement
+    
+    assert quick_and_dirty_read_file(ser, "file.txt") == original
+
+
+class TestSoftResetDirectlyIntoRepl:
+
+    def test_interrupt_existing_process(self, ser: Serial) -> None:
+        interrupt_and_enter_repl(ser)
+        
+        # Set the thing off sleeping
+        ser.write(b"import time; time.sleep(3)\r")
+        expect(ser, b"import time; time.sleep(3)\r\n")
+        
+        assert soft_reset_directly_into_repl(ser) == ""
+
+    def test_skips_main_py(self, ser: Serial) -> None:
+        interrupt_and_enter_repl(ser)
+        
+        with quick_and_dirty_override_file(ser, "main.py", "import time; time.sleep(3)"):
+            # Should have no boot message
+            assert soft_reset_directly_into_repl(ser) == ""
+            
+            # Should already be in the shell
+            ser.write(b"\r")
+            expect(ser, b"\r\n>>> ")
+
+    def test_captures_boot_py_output(self, ser: Serial) -> None:
+        interrupt_and_enter_repl(ser)
+        
+        with quick_and_dirty_override_file(ser, "boot.py", "print('booted!')"):
+            # Should have no boot message
+            assert soft_reset_directly_into_repl(ser) == "booted!\r\n"
---
+++ upyt/upy_repl.py
@@ -0,0 +1,265 @@
+"""
+Low-level utilities which interact with the MicroPython REPL.
+"""
+
+import random
+
+from serial import Serial
+
+from contextlib import contextmanager
+
+import struct
+
+
+class MicroPythonReplError(Exception):
+    """
+    Base exception thrown when the MicroPython REPL produces unexpected output.
+    
+    All exceptions include any unexpected output from the serial port as their
+    first argument.
+    """
+
+class NoReplError(MicroPythonReplError):
+    """
+    Thrown when no REPL prompt (>>>) is received when expected.
+    """
+
+class RawPasteModeNotSupportedError(MicroPythonReplError):
+    """
+    Thrown if the connected board is running a MicroPython version too old to
+    support raw paste mode.
+    """
+
+
+class SomeCodeNotSentError(MicroPythonReplError):
+    """
+    Thrown the MicroPython board fails to accept all of the code provided to
+    :py:func:`raw_paste_exec`.
+    
+    The first argument is th exception output (as raw bytes) and the second
+    argument is the ordinary output (also as raw bytes, and likely empty). The
+    third argument is the input which was never sent.
+    """
+
+
+def expect(ser: Serial, value: bytes) -> bytes:
+    """
+    Read from a serial port, checking that exactly the expected value is read.
+    If a timeout occurs, raises MicroPythonReplError.
+    
+    Returns the bytes actually read.
+    """
+    actual = ser.read(len(value))
+    if actual != value:
+        raise MicroPythonReplError(actual)
+    return actual
+
+
+def expect_endswith(ser: Serial, value: bytes) -> bytes:
+    """
+    Read from a serial port until the supplied value is read. If a timeout
+    occurs before this, raises MicroPythonReplError.
+    
+    Returns all of the bytes read.
+    """
+    actual = ser.read_until(value)
+    if not actual.endswith(value):
+        raise MicroPythonReplError(actual)
+    return actual
+
+
+def interrupt_and_enter_repl(ser: Serial, num_attempts: int = 2, timeout: float = 0.1) -> bytes:
+    """
+    Attempt to perform a keyboard interrupt to get to a REPL.
+    
+    Raises NoReplError if the REPL is not reached.
+    
+    Returns all ignored terminal output prior to the final newline and prompt
+    ('>>>'), e.g.  KeyboardInterrupt exception trace-backs).
+    
+    Makes num_attempts attempts to interrupt the running process, waiting
+    timeout seconds between each attempt. Multiple attempts are necessary if,
+    for example, an exception handler also blocks.
+    """
+    # Flush any pending input (e.g. previous command outputs or prompts)
+    unmatched_output = b""
+    if ser.in_waiting:
+        unmatched_output += ser.read(ser.in_waiting)
+    
+    # Attempt to reach a prompt follwing a keyboard interrupt
+    old_timeout = ser.timeout
+    ser.timeout = timeout
+    try:
+        for attempt in range(num_attempts):
+            ser.write(b"\x03")
+            
+            # Wait for the first hint hint of a prompt
+            prompt = b"\r\n>>> "
+            try:
+                unmatched_output += expect_endswith(ser, prompt)[:-len(prompt)]
+            except MicroPythonReplError as e:
+                # No sign of a prompt, try interrupting again as the exception
+                # handler may need interrupting
+                unmatched_output += e.args[0]
+                continue
+            
+            # To make sure what we're seeing is not just an old prompt hanging
+            # about in the buffer, produce some unique output
+            random_number = random.randint(0x10, 0xFFFFFF)
+            ser.write(b"0x%x\r" % random_number)
+            expected_response = (
+                b"0x%x\r\n"  # Echo
+                b"%d\r\n" # Decimal representation
+                b">>> "  # Fresh prompt
+            ) % (random_number, random_number)
+            try:
+                unmatched_output += expect_endswith(
+                    ser, expected_response
+                )[:-len(expected_response)]
+                # Success! We're in sync and sat waiting at a prompt!
+                return unmatched_output
+            except MicroPythonReplError as e:
+                # Fail: we didn't see our reply, lets try agian
+                unmatched_output += e.args[0]
+                continue
+        
+        # Ran out of retries
+        raise NoReplError(unmatched_output)
+    finally:
+        ser.timeout = old_timeout
+
+
+@contextmanager
+def raw_mode(ser: Serial):
+    """
+    Context manager which enters raw mode on entry and leaves raw mode again on
+    exit.
+    """
+    # Enter raw mode
+    ser.write(b"\x01")  # (Ctrl+A)
+    expect_endswith(ser, b"raw REPL; CTRL-B to exit\r\n>")
+    
+    try:
+        yield
+    finally:
+        ser.write(
+            b"\x04"  # (Ctrl+D)  End current code block (if any)
+            b"\x02"  # (Ctrl+B)  Exit raw mode
+        )
+        expect_endswith(ser, b"\r\n>>> ")
+
+
+def raw_paste_exec(ser: Serial, code: str) -> tuple[str, str]:
+    """
+    Execute the supplied code (via raw paste mode).
+    
+    Must be in raw mode (see :py:func:`raw_mode`) when calling this function.
+    
+    Names defined by the executed code remain in scope until the surrounding
+    raw mode is exited.
+    
+    The supplied code must not contain, or print a 0x04 character (Ctrl+D)
+    otherwise the raw paste mode protocol will become out-of-sync and undefined
+    behaviour will ensue.
+    
+    Returns two strings: the output of the executed code and any exceptions
+    produced.
+    
+    The executed command must complete and have sent all of its output within
+    the serial port's timeout, otherwise an error will occur.
+    """
+    # Enter raw paste mode
+    ser.write(
+        b"\x05"  # (Ctrl+E / ENQ)
+        b"A"
+        b"\x01"  # (Ctrl+A)
+    )  # (Ctrl+A)
+    response = ser.read(2)
+    if response != b"R\x01":
+        raise RawPasteModeNotSupportedError(response)
+    
+    window_size_increment = struct.unpack("<H", ser.read(2))[0]
+    window_size = window_size_increment
+  
+    code_utf8 = code.encode("utf-8")
+    if b"\x04" in code_utf8:
+        raise ValueError("Cannot eval strings containing ASCII 0x04 (ctrl+D)")
+    
+    # Send all of the code and wait for the window to re-open again
+    #
+    # NB: We need to wait for the window to reopen before sending the
+    # end-of-code if we happened to use up the whole window with our final
+    # block of code, otherwise the end-of-code might overrun the buffer!
+    while code_utf8 or window_size == 0:
+        if window_size == 0:
+            response = ser.read(1)
+            if response == b"\x01":
+                window_size += window_size_increment
+            elif response == b"\x04":
+                # Device doesn't want any more data
+                break
+            else:
+                raise MicroPythonReplError(response)
+        
+        # Send (up-to) the maxmimum allowed window worth of data
+        written = ser.write(code_utf8[:window_size])
+        code_utf8 = code_utf8[written:]
+        window_size -= written
+    
+    # End transmission
+    ser.write(b"\x04")  # (Ctrl+D)
+    while True:
+        response = ser.read(1)
+        if response == b"\x01":
+            # A window size increment, but we don't care about those anymore
+            continue
+        elif response == b"\x04":
+            # End of transmission was acknowledged
+            break
+        else:
+            raise MicroPythonReplError(response)
+    
+    # Read the response
+    code_output = expect_endswith(ser, b"\x04")[:-1]
+    exception_output = expect_endswith(ser, b"\x04")[:-1]
+    expect_endswith(ser, b">")  # Should return to raw-repl shell
+    
+    if not code_utf8:
+        # Success: All bytes were accepted!
+        return (code_output.decode("utf-8"), exception_output.decode("utf-8"))
+    else:
+        # Failure: Some bytes not received
+        raise SomeCodeNotSentError(exception_output, code_output, code_utf8)
+
+
+def soft_reset_directly_into_repl(ser: Serial) -> str:
+    """
+    Interupt any running process and perform a soft reset such that the device
+    boots directly into the REPL without executing main.py.
+    
+    Returns any output produced by boot.py as a string. Output prior to this is
+    discarded.
+    """
+    interrupt_and_enter_repl(ser)
+    
+    # NB: When reset in raw mode, main.py is not executed
+    with raw_mode(ser):
+        # Perform a reset
+        ser.write(b"\x04")  # (Ctrl + D)
+        
+        # Response from Raw REPL
+        expect(
+            ser,
+            (
+                b"OK\r\n"  # From Raw REPL
+                b"MPY: soft reboot\r\n"  # Boot message
+            ),
+        )
+        
+        # Ignore all output from boot.py
+        raw_repl_entry_message = b"raw REPL; CTRL-B to exit\r\n>"
+        boot_py_output = expect_endswith(
+            ser, raw_repl_entry_message
+        )[:-len(raw_repl_entry_message)]
+    
+    return boot_py_output.decode("utf-8")
